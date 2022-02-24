918f528209f086837f095d77bf6b95648037aa01
mlysy <mlysy@users.noreply.github.com>
2022-02-24 13:16:22 -0500

    fixed link in vignette

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -5,6 +5,11 @@
 
 using namespace Rcpp;
 
+#ifdef RCPP_USE_GLOBAL_ROSTREAM
+Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
+Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
+#endif
+
 // hadamard
 SEXP hadamard(NumericMatrix A, NumericMatrix B);
 RcppExport SEXP _TMBtools_hadamard(SEXP ASEXP, SEXP BSEXP) {
